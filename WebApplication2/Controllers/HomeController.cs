9f1a7d7f02035fccaf39184f89c42fcc17d35598
Aleksei Kogtev <akogtev@gmail.com>
2016-11-04 00:54:46 +1000

    first

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StudentManager()
+        {
+            return PartialView("NewStudent");
+        }
+        public ActionResult NewStudent()
+        {
+            return PartialView();
+        }
+        //
+        // POST: 
+        [HttpPost]
+        public ActionResult NewStudent(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction("Index","Manage");
+            }
+            return null;
+        }
+    }
+}
---
+++ WebApplication2/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class Student
+    {
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+
+        public int CourseID { get; set; }
+
+        public int LocationID { get; set; }
+    }
+}
